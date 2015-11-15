044a3cbe180d270482ee746a48d265a512d61c46
Conor O'Kelly <okellyconor@gmail.com>
2015-11-15 23:18:51 +0000

    Finished Lab 15|

---
+++ Lab 15/P15P3.py
@@ -0,0 +1,54 @@
+__author__ = 'conor'
+
+"""
+Psuedocode
+
+def series(number):
+    if number == 1:
+        return 2
+    else:
+        print "doing number x"
+        return number - series(number - 1)
+
+def get_input():
+    get input
+
+def get_series():
+
+    check above 0
+
+    if yes run function
+
+    elif exit
+
+"""
+
+def series(number):
+    if number == 0:
+        print("Currently calculating for number 0. Last one \n")
+        return 13
+    elif number == 1:
+        print ("Currently calculating for number 1")
+        return 8
+    else:
+        print ("Currently calculating for number %i" % (number))
+        return ( (series( (number-2) )) + (13) * (series( (number-1) )) )
+
+def get_input():
+    number = int(raw_input("Please enter a number for the function (int) \n"))
+    return number
+
+def get_series():
+    number = get_input()
+
+    while number >= 0:
+        print ("The result is %i \n" % (series(number)))
+        number = get_input()
+
+    else:
+        print("The number is below 0. Exiting")
+    return
+
+
+if __name__ == '__main__':
+    get_series()
